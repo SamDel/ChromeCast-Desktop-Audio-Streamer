c0076b0b731da1e4b6ab774ca812d0540ad5d59d
SamDel <github@deaut.nl>
2018-02-25 07:42:19 +0100

    Get the friendly name of the device, to show in the interface.

--- Source/ChromeCast.Desktop.AudioStreamer/Discover/DiscoverDevices.cs
+++ Source/ChromeCast.Desktop.AudioStreamer/Discover/DiscoverDevices.cs
@@ -4,6 +4,9 @@ using Rssdp;
 using ChromeCast.Desktop.AudioStreamer.Discover.Interfaces;
 using System.Linq;
 using Zeroconf;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace ChromeCast.Desktop.AudioStreamer.Discover
 {
@@ -50,10 +53,33 @@ namespace ChromeCast.Desktop.AudioStreamer.Discover
             if (protocol.StartsWith("_googlecast"))
             {
                 onDiscovered(
-                    new DiscoveredSsdpDevice { DescriptionLocation = new Uri($"http://{ipAddress}") },
-                    new SsdpRootDevice { FriendlyName = ipAddress }
+                    new DiscoveredSsdpDevice { DescriptionLocation = new Uri($"http://{ipAddress}"), Usn = ipAddress },
+                   new SsdpRootDevice { FriendlyName = GetDeviceFriendlyNameAsync(ipAddress).Result }
                 );
             }
         }
+
+        private async Task<string> GetDeviceFriendlyNameAsync(string ipAddress)
+        {
+            var friendlyName = "";
+            try
+            {
+                var http = new HttpClient();
+                var responce = await http.GetAsync($"http://{ipAddress}:8008/setup/eureka_info?options=detail");
+                var json = await responce.Content.ReadAsStringAsync();
+                var info = JsonConvert.DeserializeObject<EurekaInfo>(json);
+                friendlyName = info.Name;
+            }
+            catch (Exception)
+            {
+                friendlyName = ipAddress;
+            }
+            return friendlyName;
+        }
+    }
+
+    public class EurekaInfo
+    {
+        public string Name { get; set; }
     }
 }
